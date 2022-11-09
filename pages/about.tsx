1d106281ba15623f783ec8c2f58486b8fadd472c
Supreme-Trinity <supremetrinity151@gmail.com>
2022-11-09 11:02:36 +0530

    first commit

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div  className='flex w-full h-full absolute left-0 top-[60px] lg:top-[70px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center'>
+        About Page
+    </div>
+  )
+}
+
+export default about
---
+++ utils/constants.tsx
@@ -0,0 +1,45 @@
+import { BsCode, BsEmojiSunglasses } from 'react-icons/bs';
+import { GiCakeSlice, GiGalaxy, GiLipstick } from 'react-icons/gi';
+import { FaPaw, FaMedal, FaGamepad } from 'react-icons/fa';
+import {SiYoutubegaming } from 'react-icons/si';
+import {GiDistraction} from 'react-icons/gi';
+
+
+export const topics = [
+  {
+    name: 'Action',
+    icon: <GiDistraction />,
+  },
+  {
+    name: 'comedy',
+    icon: <BsEmojiSunglasses />,
+  },
+  {
+    name: 'gaming',
+    icon: <FaGamepad />,
+  },
+  {
+    name: 'food',
+    icon: <GiCakeSlice />,
+  },
+  {
+    name: 'dance',
+    icon: <GiGalaxy />,
+  },
+  {
+    name: 'beauty',
+    icon: <GiLipstick />,
+  },
+  {
+    name: 'animals',
+    icon: <FaPaw />,
+  },
+  {
+    name: 'sports',
+    icon: <FaMedal />,
+  },
+];
+
+export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Carrers', 'ByteDance', 'Creator Directory']
+export const footerList2 = [ 'TikTik for Good','Advertise','Developers','Transparency','TikTik Rewards' ]
+export const footerList3 = [ 'Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines' ]
